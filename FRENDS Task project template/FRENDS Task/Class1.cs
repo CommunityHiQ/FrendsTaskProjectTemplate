3298e8b4f7a9bdef7067a5f21e1dcf24730e4ca7
Jefim Borissov <jefim.borissov@gmail.com>
2017-07-07 13:30:18 +0300

    Initial commit

---
+++ FRENDS Task project template/FRENDS Task/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace $safeprojectname$
+{
+    public class Class1
+    {
+        public class Parameters
+        {
+            /// <summary>
+            /// This comment will be displeyd as tool tip at UI.
+            /// </summary>
+            //[DisplayName("SampleMemberValue")]
+            //public <type> MemberName { get; set; }
+        }
+
+        /// <summary>
+        /// Options for the call
+        /// </summary>
+        public class Options
+        {
+            /// <summary>
+            /// This comment will be displeyd as tool tip at UI.
+            /// </summary>
+            //[DefaultValue("\"\"")]
+            //public string SampleProperty { get; set; }
+        }
+
+        public class Output
+        {
+            //TODO: Implement output object here.
+        }
+
+        public class Input
+        {
+            //TODO: Implement input fields here.
+            //[DefaultValue("\"\"")]
+            //public string SampleProperty { get; set; }
+        }
+
+        public class Execute
+        {
+            //Sample task method.
+            public Output Task(Input input, Parameters parameters, Options options)
+            {
+                //TODO: implement task business logic here.
+                return new Output();
+            }
+        }
+    }
+}
